c17443a9819f8dd1504386bb1199461270509825
Kevin Tursi <kevin.tursi@uconn.edu>
2023-05-02 15:58:21 -0400

    Added filteredExpenses() to have filter expenses by year in dropdown

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,17 +5,24 @@ import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2021');
+    const [filteredYear, setFilteredYear] = useState('2020');
 
     const dropdownHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
     return (
         <div>
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onFilterChange={dropdownHandler}/>
-            {props.items.map((expense) => (
+            <ExpensesFilter 
+                selected={filteredYear} 
+                onFilterChange={dropdownHandler}
+            />
+            {filteredExpenses.map((expense) => (
                 <ExpenseItem 
                     key={expense.id}
                     title={expense.title} 
