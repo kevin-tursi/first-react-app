ddba13e7b86aafafc3f5a64c4f7a9e3f14d4b071
Kevin Tursi <kevin.tursi@uconn.edu>
2023-05-02 15:46:41 -0400

    Added key to ExpenseItem for unique identifier

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,6 +17,7 @@ const Expenses = (props) => {
             <ExpensesFilter selected={filteredYear} onFilterChange={dropdownHandler}/>
             {props.items.map((expense) => (
                 <ExpenseItem 
+                    key={expense.id}
                     title={expense.title} 
                     amount={expense.amount} 
                     date={expense.date} 
